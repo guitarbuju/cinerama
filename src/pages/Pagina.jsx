deedeef79f4f5a71d44f8c93dbc4561b2f8becfc
guitarbuju <gerardoamadrid@gmail.com>
2024-09-10 13:05:23 +0200

    back to dotenv

--- src/pages/Pagina.jsx
+++ src/pages/Pagina.jsx
@@ -11,7 +11,7 @@ import Header from "../components/Header";
 import styles from "./movie.module.css";
 import Pager from "../components/Pager";
 import MovieListButtons from "../components/MovieListButtons";
-// import { TMDB_API_KEY , TMDB_URL_ENDPOINT } from "../lib/env_imports";
+import { TMDB_API_KEY , TMDB_URL_ENDPOINT } from "../lib/env_imports";
 
 
 const Pagina = () => {
@@ -45,8 +45,8 @@ const Pagina = () => {
   useEffect(() => {
     const cargarPeliculas = async () => {
       try {
-        // const url= `${TMDB_URL_ENDPOINT}${movieList}?api_key=${TMDB_API_KEY}&language=esp-MX&page=${actualPage}`
-         const url= `https://api.themoviedb.org/3/movie/${movieList}?api_key=7c93a29133df64c786e0131de31c666c&language=esp-MX&page=${actualPage}`
+        const url= `${TMDB_URL_ENDPOINT}${movieList}?api_key=${TMDB_API_KEY}&language=esp-MX&page=${actualPage}`
+         //const url= `https://api.themoviedb.org/3/movie/${movieList}?api_key=7c93a29133df64c786e0131de31c666c&language=esp-MX&page=${actualPage}`
 
         const respuesta = await axios.get(url);
         const results = respuesta.data.results;
