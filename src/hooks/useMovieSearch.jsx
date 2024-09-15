741223a28b0300d3352b6280954135ec9038901d
guitarbuju <gerardoamadrid@gmail.com>
2024-09-15 18:40:34 +0200

    adapted base url for search

--- src/hooks/useMovieSearch.jsx
+++ src/hooks/useMovieSearch.jsx
@@ -1,13 +1,14 @@
 import axios from "axios";
 import { useState } from "react";
-import { TMDB_API_KEY, TMDB_URL_ENDPOINT } from "../lib/env_imports";
+import { TMDB_API_KEY, TMDB_URL_SEARCH_ENDPOINT } from "../lib/env_imports";
 
 const useMovieSearch = () => {
+  
   const [movieSearch, setMovieSearch] = useState("");
   const [movieData, setMovieData] = useState([]);
   const [image, setImage] = useState(true);
 
-  const url = ` ${TMDB_URL_ENDPOINT}/search/movie?api_key=${TMDB_API_KEY}&query=${movieSearch}&include_adult=false&language=en-US&page=1`;
+  const url = ` ${TMDB_URL_SEARCH_ENDPOINT}?api_key=${TMDB_API_KEY}&query=${movieSearch}&include_adult=false&language=en-US&page=1`;
 
   // const url = `https://api.themoviedb.org/3/search/movie?api_key=7c93a29133df64c786e0131de31c666c&query=${movieSearch}&include_adult=false&language=en-US&page=1`;
 
--- src/lib/env_imports.js
+++ src/lib/env_imports.js
@@ -1,4 +1,5 @@
 
 
 export const TMDB_URL_ENDPOINT =import.meta.env.VITE_BASE_URL;
+export const TMDB_URL_SEARCH_ENDPOINT =import.meta.env.VITE_SEARCH_BASE_URL;
 export const TMDB_API_KEY= import.meta.env.VITE_API_KEY;
