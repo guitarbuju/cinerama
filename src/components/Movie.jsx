231774946644e26588942a2c3c5b8bf07dbf452a
guitarbuju <gerardoamadrid@gmail.com>
2024-09-16 15:37:17 +0200

    fixed title

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -12,6 +12,11 @@ const Movie = ({ movie }) => {
         key={movie.id}
         className="flex-col justify-center items-center sm:flex-row sm:justify-center sm:align-middle rounded-lg p-4 bg-stone-900"
       >
+      <h1 className=" text-zinc-300 font-sans sm:text-md mb-2"
+     style={{textShadow:"1px 1px 1px rgb(24, 24, 27)"}}
+     >
+            {movie.title}
+          </h1>
         <motion.div
           className="rounded flex justify-center"
           initial={{ opacity: 0, scale: 0.1 }}
@@ -26,9 +31,7 @@ const Movie = ({ movie }) => {
           />
         </motion.div>
         <div className="sm:flex sm:flex-col p-2 justify-center items-center mt-2">
-          <h1 className=" text-white font-sans text-lg sm:text-sm">
-            {movie.title}
-          </h1>
+          
           <div className="overflow-hidden -mt-4 justify-center align-middle">
             <p className=" text-white text-sm text-left p-3 w-80  h-20 sm:w-44 md:w-52 lg:w-60 sm:text-xs justify-center align-middle antialiased ">
               {movie.overview}
